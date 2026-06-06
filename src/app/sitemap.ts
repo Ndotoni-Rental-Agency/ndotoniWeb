1b5263f87582feed448e41eff1994ad851659bae
Emmanuel Makoye <makoye@Emmanuels-MacBook-Air.local>
2026-06-06 11:15:45 -0700

    seo: add /refer to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -134,6 +134,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'weekly',
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/refer`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
   ]
 
   // Blog post pages
